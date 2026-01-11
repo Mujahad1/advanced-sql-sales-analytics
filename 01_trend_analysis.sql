73394f904774fc4b7d07ebdeff3fe6cffc3dcaad
Mujahad1 <mujahadahmed6@gmail.com>
2026-01-11 17:21:23 +0530

    Update 01_trend_analysis.sql

--- 01_trend_analysis.sql
+++ 01_trend_analysis.sql
@@ -1,1 +1,28 @@
+-- =====================================================
+-- Trend & Seasonality Analysis
+-- Objective:
+-- Analyze how sales and quantity evolve over time
+-- =====================================================
+
+-- Daily trend
+SELECT 
+    order_date,
+    SUM(sales_amount) AS total_sales,
+    SUM(quantity) AS total_quantity
+FROM dbo.gold.fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY order_date
+ORDER BY order_date;
+
+
+-- Monthly / Yearly trend using DATETRUNC (Best Practice)
+SELECT
+    DATETRUNC(YEAR, order_date) AS order_year,
+    SUM(sales_amount) AS total_sales,
+    COUNT(DISTINCT customer_key) AS total_customers,
+    SUM(quantity) AS total_quantity
+FROM dbo.gold.fact_sales
+WHERE order_date IS NOT NULL
+GROUP BY DATETRUNC(YEAR, order_date)
+ORDER BY order_year;
 
