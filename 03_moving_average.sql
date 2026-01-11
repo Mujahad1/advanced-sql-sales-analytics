0412bcd7155cb7a78ae3fb10d656eea9ccdd2273
Mujahad1 <mujahadahmed6@gmail.com>
2026-01-11 17:22:27 +0530

    Update 03_moving_average.sql

--- 03_moving_average.sql
+++ 03_moving_average.sql
@@ -1,1 +1,22 @@
+-- =====================================================
+-- Running Total & Moving Average Analysis
+-- Objective:
+-- Smooth sales trends and reduce short-term volatility
+-- =====================================================
+
+SELECT 
+    order_year,
+    total_sales,
+    SUM(total_sales) OVER (ORDER BY order_year) AS running_total,
+    AVG(avg_price) OVER (ORDER BY order_year) AS moving_avg_price
+FROM (
+    SELECT 
+        DATETRUNC(YEAR, order_date) AS order_year,
+        SUM(sales_amount) AS total_sales,
+        AVG(price) AS avg_price
+    FROM dbo.gold.fact_sales
+    WHERE order_date IS NOT NULL
+    GROUP BY DATETRUNC(YEAR, order_date)
+) t
+ORDER BY order_year;
 
