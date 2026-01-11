a772570957cfe1d8337abbfc6b185490243a7a6d
Mujahad1 <mujahadahmed6@gmail.com>
2026-01-11 17:21:59 +0530

    Update 02_cumulative_analysis.sql

--- 02_cumulative_analysis.sql
+++ 02_cumulative_analysis.sql
@@ -1,1 +1,20 @@
+-- =====================================================
+-- Cumulative Sales Analysis
+-- Objective:
+-- Evaluate long-term business growth using running totals
+-- =====================================================
+
+SELECT 
+    order_month,
+    total_sales,
+    SUM(total_sales) OVER (ORDER BY order_month) AS running_total
+FROM (
+    SELECT 
+        DATETRUNC(MONTH, order_date) AS order_month,
+        SUM(sales_amount) AS total_sales
+    FROM dbo.gold.fact_sales
+    WHERE order_date IS NOT NULL
+    GROUP BY DATETRUNC(MONTH, order_date)
+) t
+ORDER BY order_month;
 
